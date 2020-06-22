5020555afeb515e85b0073ddcd4df995f05b39c0
Piotr Kowalski <piecioshka@gmail.com>
2020-06-22 23:45:47 +0200

    Initialize project

---
+++ README.md
@@ -0,0 +1,9 @@
+# github-track-followers-setup
+
+:hammer: Installation script for [github-track-followers](https://github.com/piecioshka/github-track-followers)
+
+## Install
+
+```bash
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+dirname=`pwd`
+directory="${dirname}/followers"
+filename="${dirname}/bin/dump.sh"
+user='piecioshka'
+dump="$directory/$user-\$(date +'%Y-%m-%d-%H-%M-%S').md"
+
+# Create directory for dumps
+mkdir -p $directory
+
+# Create script
+cat > $filename <<EOL
+#!/usr/bin/env bash
+
+npx github-track-followers -u $user > $dump
+EOL
+
+# Make it executable
+chmod +x $filename
